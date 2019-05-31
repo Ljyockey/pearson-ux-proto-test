a8ae2ab5eaf381aa5ff03c6f5f64d0e8e107e6a2
ljyockey <leoyockey@gmail.com>
2019-05-31 01:45:07 -0700

    Part III: JavaScript - Make a list - initial commit

---
+++ makeAList.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const getOddOrEven = value => value % 2 === 0 ? 'EVEN' : 'ODD';
+
+const getIfMultipleOf = (target, value) => value % target === 0 ? ` ${target} MULTIPLE` : '';
+
+const makeAList = size => {
+  const ul = document.createElement('ul');
+  for (let i = 0; i < size; i ++) {
+    const element = document.createElement('li');
+    element.id = `list-item--${i}`;
+    element.innerText = `${getOddOrEven(i)}${getIfMultipleOf(3, i)}${getIfMultipleOf(5, i)}`;
+    ul.appendChild(element);
+  }
+  document.getElementById('list').appendChild(ul);
+};
+
+module.exports = makeAList;
