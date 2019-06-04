94dd04e165a9c52a20c880fc740366678f5c3f81
ljyockey <leoyockey@gmail.com>
2019-06-04 10:50:10 -0700

    Part IV - Prototyping - Video Quiz - form empty state styling

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -35,7 +35,7 @@ export default class Quiz extends React.Component {
                     <video controls src={this.props.videoSrc}></video>
                     <QuizForm
                         onFormSubmit={this.onFormSubmit}
-                        currentQuestion={this.props.questions[3]}
+                        currentQuestion={this.props.questions[this.state.currentQuestionIndex]}
                         questionNumber={this.state.currentQuestionIndex ? this.state.currentQuestionIndex + 1 : null}
                     />
                 </div>
