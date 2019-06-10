775013f09661bcc352cbdec9b93340301010c8cc
ljyockey <leoyockey@gmail.com>
2019-06-09 17:50:35 -0700

    Part IV - Prototyping - Video Quiz - QuizResults component initial

--- src/components/QuizResults.jsx
+++ src/components/QuizResults.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
 export default function QuizResults (props) {
-  return 'QuizResults';
+  const {totalCorrect, totalQuestions} = props;
+  const totalWrong = totalQuestions - totalCorrect;
+  const score = parseInt((totalCorrect / totalQuestions).toFixed(2).slice(2));
+
+  return (
+    <div className={'c-quizresults--root'}>
+      <div className={'svg-container'}>
+        <svg viewBox="0 0 42 42" className={'svg-score'}>
+          <circle className="wrong" cx="21" cy="21" r="15.91549430918954" fill="transparent" strokeWidth="3"></circle>
+          <circle className="correct" cx="21" cy="21" r="15.91549430918954" fill="transparent" strokeWidth="3" strokeDasharray={`${score} ${100 - score}`} strokeDashoffset="0"></circle>
+          <text aria-describedby={'score-description'} x="21" y="25" fontSize="smaller" textAnchor="middle">{score + '%'}</text>
+        </svg>
+        <p id="score-description">Your quiz score!</p>
+      </div>
+      <ul>
+        <li>{totalQuestions} Questions</li>
+        <li>{totalCorrect} Correct</li>
+        <li>{totalWrong} Incorrect</li>
+        <li>Score {score + '%'}</li>
+      </ul>
+    </div>
+  );
 }
