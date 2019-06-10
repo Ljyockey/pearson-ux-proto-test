db3df4e5639e728fd08e9132d320f35b95cecbd4
ljyockey <leoyockey@gmail.com>
2019-06-09 19:55:45 -0700

    Part II - SASS - initial commit

---
+++ src/components/MixinTest.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import '../scss/components/MixinTest/MixinTest.scss';
+
+export default function MixinTest () {
+  return (
+    <ul>
+      <li>Foo</li>
+      <li>Foo</li>
+      <li>Foo</li>
+      <li>Foo</li>
+      <li>Foo</li>
+      <li>Foo</li>
+      <li>Foo</li>
+      <li>Foo</li>
+      <li>Foo</li>
+    </ul>
+  );
+}
