bf1d913e3b36efcc44fd9a4ed15a1b42387d5d94
ljyockey <leoyockey@gmail.com>
2019-06-14 21:24:05 -0700

    Part IV - Prototyping - Video Quiz - Dropdown html initial

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,25 +1,56 @@
 import React from 'react';
 
-export default function Dropdown (props) {
-  const {onQuestionChange, answerIndexes, currentQuestionIndex} = props;
+export default class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const generateListItems = () => {
+    this.state = {
+      isOpen: false
+    };
+
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  toggleOpen () {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  onQuestionChange (index) {
+    this.props.onQuestionChange(index);
+    this.toggleOpen();
+  }
+
+  render () {
+    const {answerIndexes, currentQuestionIndex} = this.props;
+
+    const generateListItems = () => {
     // using a for loop instead of map because the array values are empty initially
     // and using Array.fill isn't ie11-compatible 
-    const result = [];
-    for (let i=0; i < answerIndexes.length; i++) {
-      const answer = answerIndexes[i];
-      result.push(
-        <li key={i} onClick={() => onQuestionChange(i)}>
-          {!!answer || answer === 0 ? 'Complete' : 'Incomplete'}
-        </li>
-      );
-    }
-    return result;
-  };
+      const result = [];
+      for (let i=0; i < answerIndexes.length; i++) {
+        const answer = answerIndexes[i];
+        result.push(
+          <li
+            key={i}
+            id={i}
+            role={'option'}
+          >
+            <button onClick={() => this.onQuestionChange(i)}>
+              {i+1}. {!!answer || answer === 0 ? 'Complete' : 'Incomplete'}
+            </button>
+          </li>
+        );
+      }
+      return result;
+    };
 
-  return <div className={'justify-flex-end'}>
-    <p>Question {currentQuestionIndex + 1} of {answerIndexes.length}</p>
-    <ol>{generateListItems()}</ol>;
-  </div>;
+    return <div className={'c-dropdown--root justify-flex-end'}>
+      <button aria-haspopup={'listbox'} onClick={this.toggleOpen}>
+        Question {currentQuestionIndex + 1} of {answerIndexes.length}
+      </button>
+      {this.state.isOpen && <ol role={'listbox'}>{generateListItems()}</ol>}
+    </div>;
+  }
 }
