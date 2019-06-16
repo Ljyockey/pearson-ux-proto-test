8b3d9c90d15f47fe500425cea72fb250dfd69f77
ljyockey <leoyockey@gmail.com>
2019-06-16 13:05:05 -0700

    Part IV - Prototyping - Video Quiz - focus modal on open

--- src/components/QuizModal.jsx
+++ src/components/QuizModal.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 
-export default function QuizModal (props) {
-  const {
-    className,
-    title,
-    onCloseCallback,
-    message,
-    cancelButton,
-    confirmButton,
-    hasAutoFocus
-  } = props;
+export default class QuizModal extends React.Component {
+  componentDidMount () {
+    document.getElementById('close-modal').focus();
+  }
 
-  return (
-    <section className={`c-quizmodal--root ${className || ''}`} role={'region'} autoFocus={!!hasAutoFocus}>
-      <div className={'header'}>
-        <h4 className={'title'}>{title}</h4>
-        <button className={'close'} onClick={onCloseCallback}>{String.fromCharCode(10007)}</button>
-      </div>
-      <div className={'main'}>
-        <p className={'message'}>{message}</p>
-        <div className={'action-items'}>
-          {cancelButton && <button onClick={cancelButton.onClickCallback} className={'cancel'}>{cancelButton.text}</button>}
-          {confirmButton && <button onClick={confirmButton.onClickCallback} className={'confirm'}>{confirmButton.text}</button>}
+  render () {
+    const {
+      className,
+      title,
+      onCloseCallback,
+      message,
+      cancelButton,
+      confirmButton,
+      hasAutoFocus
+    } = this.props;
+
+    return (
+      <section className={`c-quizmodal--root ${className || ''}`} role={'region'} autoFocus={!!hasAutoFocus}>
+        <div className={'header'}>
+          <h4 className={'title'}>{title}</h4>
+          <button id={'close-modal'} className={'close'} onClick={onCloseCallback}>{String.fromCharCode(10007)}</button>
         </div>
-      </div>
-    </section>
-  );
+        <div className={'main'}>
+          <p className={'message'}>{message}</p>
+          <div className={'action-items'}>
+            {cancelButton && <button onClick={cancelButton.onClickCallback} className={'cancel'}>{cancelButton.text}</button>}
+            {confirmButton && <button onClick={confirmButton.onClickCallback} className={'confirm'}>{confirmButton.text}</button>}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
