3ad3f22b474ebb10ca7fae673653906215f760a2
ljyockey <leoyockey@gmail.com>
2019-06-15 19:20:35 -0700

    Part IV - Prototyping - Video Quiz - Accordion styling

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -53,6 +53,16 @@ export default class Accordion extends React.Component {
     }
   }
 
+  generateOptionList (options, correctAnswerIndex, userAnswerIndex) {
+    return options.map((o, i) => (
+      <li
+        key={i}
+        className={`option${i === correctAnswerIndex ? ' option--correct': ''}${i === userAnswerIndex && i !== correctAnswerIndex ? ' option--incorrect' : ''}`}>
+        {o}
+      </li>
+    ));
+  }
+
   render () {
     return <div className={'c-accordion--root'} onKeyDown={this.onKeyDown}>
       {this.props.questions.map((q, i) => (
@@ -65,11 +75,15 @@ export default class Accordion extends React.Component {
               id={`accordion-header-${i}`}
               className={'accordion-header'}
             >
-            Question {i+1} [{this.getVideoTime(q.timeInSeconds)}]
+              <span className={`triangle-bullet${this.state.openIndex === i ? ' triangle-bullet--expanded' : ''}`}>{String.fromCharCode(8227)}</span> Question {i+1} <span className={'time'}> [{this.getVideoTime(q.timeInSeconds)}]</span>
+              <span className={`feedback feedback--${q.correctAnswerIndex === this.props.answers[i] ? 'correct' : 'incorrect'}`}>{q.correctAnswerIndex === this.props.answers[i] ? String.fromCharCode(10003) : String.fromCharCode(10008)}</span>
             </button>
           </h3>
-          {this.state.openIndex === i && <div id={`accordion-item-${i}`}>
+          {this.state.openIndex === i && <div className={'accordion-item'} id={`accordion-item-${i}`}>
             <p>{q.question}</p>
+            <ul>
+              {this.generateOptionList(q.options, q.correctAnswerIndex, this.props.answers[i])}
+            </ul>
           </div>}
         </div>
       ))}
